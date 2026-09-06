8fbf287b3048b81a5d15048a0016776f35d05c89
azad2022 <95263596+azad2022@users.noreply.github.com>
2026-09-06 17:16:52 +0330

    feat(pay): export route and state boundary helpers

--- src/pay/index.ts
+++ src/pay/index.ts
@@ -1,7 +1,9 @@
 export { default as PayApp } from './PayApp';
 export { default as PayCheckout } from './PayCheckout';
+export { default as PayDataStateView } from './DataStateView';
 export * from './types';
 export * from './i18n';
 export * from './routing';
+export * from './route-match';
 export * from './data-state';
 export * from './http';
